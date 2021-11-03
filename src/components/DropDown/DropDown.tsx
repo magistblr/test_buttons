bdd74ed9c682399eed3f26f54f6c4986b1d2c5f3
magistblr <78726323+magistblr@users.noreply.github.com>
2021-11-03 12:01:20 +0300

    refactor

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -16,7 +16,6 @@ export const DropDown: React.FC<DropDownType & ButtonType> = ({ children, ...pro
     setDrop(!drop);
   };
 
-
   const handleOutsideClick  = (event:any) => {
       const path = event.path;
       if (!path.includes(sortRef.current)) {
@@ -31,12 +30,12 @@ export const DropDown: React.FC<DropDownType & ButtonType> = ({ children, ...pro
 
 
   return (
-    <div className="dropDown" ref={sortRef}>
-      <Button callback={changeDropHandler} type={type} disabled={disabled} loading={loading} min={min} large={large} block={block} outlined={outlined} text={text}>{children}</Button>
-        <div className="dropDown__items-wrapper" >
-              <ul  className={`dropDown__items ${drop && "dropDown__items_active"}`}>
+    <div className="dropDown" ref={sortRef} onClick={changeDropHandler}>
+      <Button  callback={changeDropHandler} type={type} disabled={disabled} loading={loading} min={min} large={large} block={block} outlined={outlined} text={text}>{children}</Button>
+        <div  className="dropDown__items-wrapper" >
+              <ul className={`dropDown__items ${!drop && "dropDown__items_active"}`}>
                 {data.map((elem,i) => (
-                  <li key={i} onClick={changeDropHandler} className="dropDown__item">{elem}</li>
+                  <li key={i} onClick={changeDropHandler}  className="dropDown__item">{elem}</li>
                 ))}
               </ul>
         </div>
